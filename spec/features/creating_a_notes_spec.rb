cc1f6b57982c4f7c35912315d0103fa548d1eed7
Savannah Sosa <savannah.e.sosa@gmail.com>
2017-10-05 11:07:16 -0400

    Create test for creating a new note

---
+++ spec/features/creating_a_notes_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "CreatingANotes", type: :feature do
+  it 'allows creating a note' do
+    visit '/'
+    click_on 'New Note'
+    fill_in 'Title', with: 'New Note Title'
+    fill_in 'Body', with: 'New Note Body'
+    click_on 'create'
+  end
+end
