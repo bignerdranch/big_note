f106e37ed11a17e353cffe1de48924d553348c2e
Savannah Sosa <savannah.e.sosa@gmail.com>
2017-10-05 11:22:21 -0400

    Created notes controller and initial view to pass first test

--- spec/features/creating_a_notes_spec.rb
+++ spec/features/creating_a_notes_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "CreatingANotes", type: :feature do
   it 'allows creating a note' do
-    visit '/'
+    visit '/notes'
     click_on 'New Note'
     fill_in 'Title', with: 'New Note Title'
     fill_in 'Body', with: 'New Note Body'
