dfb9b00ec6f806f7afce02a81c625f0955d04646
Savannah Sosa <savannah.e.sosa@gmail.com>
2017-11-17 14:52:04 -0500

    Added create note screen and list of notes on index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,22 @@
 class NotesController < ApplicationController
 
   def index
-    
+    @notes = Note.all
+  end
+
+  def new
+  	@note = Note.new
+  end 
+
+  def create
+  	Note.create(note_params)
+  	redirect_to notes_path
+  end 
+
+  private 
+
+  def note_params
+  	params.require(:note).permit(:title, :body)
   end
 
 end
--- spec/features/creating_a_notes_spec.rb
+++ spec/features/creating_a_notes_spec.rb
@@ -7,5 +7,7 @@ RSpec.feature "CreatingANotes", type: :feature do
     fill_in 'Title', with: 'New Note Title'
     fill_in 'Body', with: 'New Note Body'
     click_on 'create'
+    expect(page).to have_current_path('/notes')
+    expect(page).to have_content('New Note Title')
   end
 end
