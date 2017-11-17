b20fd3ba5bbd42aab020e519bfe3e9a2f6335dd5
Savannah Sosa <savannah.e.sosa@gmail.com>
2017-11-17 14:57:04 -0500

    Refactored tests

--- spec/features/creating_a_notes_spec.rb
+++ spec/features/creating_a_notes_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.feature "CreatingANotes", type: :feature do
   it 'allows creating a note' do
-    visit '/notes'
+    create_a_note
+  end
+
+  def create_a_note 
+  	visit '/notes'
     click_on 'New Note'
     fill_in 'Title', with: 'New Note Title'
     fill_in 'Body', with: 'New Note Body'
     click_on 'create'
     expect(page).to have_current_path('/notes')
     expect(page).to have_content('New Note Title')
-  end
+  end 
+
 end
