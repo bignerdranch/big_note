09866d6e59f7a6de7421f331f10e198c44b43ba5
Savannah Sosa <savannah.e.sosa@gmail.com>
2017-12-20 10:17:20 -0500

    Added edit functionality and tests

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,6 +13,17 @@ class NotesController < ApplicationController
   	redirect_to notes_path
   end 
 
+  def edit 
+    @note = Note.find(params[:id])
+  end 
+
+  def update 
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to edit_note_path(@note)
+    end
+  end 
+
   private 
 
   def note_params
--- spec/features/creating_a_notes_spec.rb
+++ spec/features/creating_a_notes_spec.rb
@@ -2,17 +2,32 @@ require 'rails_helper'
 
 RSpec.feature "CreatingANotes", type: :feature do
   it 'allows creating a note' do
-    create_a_note
+    note_title = 'New Note Title'
+    create_a_note(note_title)
+    edit_a_note(note_title)
   end
 
-  def create_a_note 
+  def create_a_note(note_title)
   	visit '/notes'
     click_on 'New Note'
-    fill_in 'Title', with: 'New Note Title'
+    fill_in 'Title', with: note_title
     fill_in 'Body', with: 'New Note Body'
     click_on 'create'
     expect(page).to have_current_path('/notes')
-    expect(page).to have_content('New Note Title')
+    expect(page).to have_content(note_title)
+  end 
+
+  def edit_a_note(note_title)
+    visit '/notes'
+    click_on note_title
+    fill_in 'Title', with: 'Edited note title'
+    fill_in 'Body', with: 'Edited note body'
+    click_on 'save'
+    expect(page).to have_selector('input[value="Edited note title"]')
+    expect(page).to have_content('Edited note body')
+    click_on 'close'
+    expect(page).to have_current_path('/notes')
+    expect(page).to have_content('Edited note title')
   end 
 
 end
